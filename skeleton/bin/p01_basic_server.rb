9a7f3883b7a8d23722fa290b3f306b3e685040ad
nilshen <linshen88@gmail.com>
2022-05-16 15:56:26 -0400

    w8d1 basic rack setup

--- skeleton/bin/p01_basic_server.rb
+++ skeleton/bin/p01_basic_server.rb
@@ -1,1 +1,17 @@
 require 'rack'
+
+app = Proc.new do |env|
+    req = Rack::Request.new(env)
+    res = Rack::Response.new
+    res['Content-Type'] = 'text/html'
+    res.write("Hello world!")
+    res.finish
+  end
+
+
+
+Rack::Server.start(
+  app: Proc.new do |env|
+    ['200', {'Content-Type' => 'text/html'}, ['hello world']]
+  end
+)
