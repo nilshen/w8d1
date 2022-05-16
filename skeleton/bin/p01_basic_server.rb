358901bbf9ab465b96efde266b95dd40484dba13
nilshen <linshen88@gmail.com>
2022-05-16 16:16:38 -0400

    phase1 setup rack

--- skeleton/bin/p01_basic_server.rb
+++ skeleton/bin/p01_basic_server.rb
@@ -4,14 +4,20 @@ app = Proc.new do |env|
     req = Rack::Request.new(env)
     res = Rack::Response.new
     res['Content-Type'] = 'text/html'
-    res.write("Hello world!")
+    # res.write("Hello world!")
+    res.write(req.path)
     res.finish
   end
 
 
 
+# Rack::Server.start(
+#   app: Proc.new do |env|
+#     ['200', {'Content-Type' => 'text/html'}, ['hello world']]
+#   end
+# )
+
 Rack::Server.start(
-  app: Proc.new do |env|
-    ['200', {'Content-Type' => 'text/html'}, ['hello world']]
-  end
+  app: app,
+  Port: 3000
 )
